0ced9a2862812a5e4570109105a69abf9ac2e998
Kristina Shauchenka <kristinashevchenko03@gmail.com>
2020-09-20 20:19:55 +0300

    support for rock-paper-scissors game, formatted code

--- chat/server.py
+++ chat/server.py
@@ -1,32 +1,56 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import datetime
+import random
 
 clients = {}
 addresses = {}
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 4203
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
+
+GAME_WIN = {
+                ('paper', 'rock'),
+                ('rock', 'scissors'),
+                ('scissors', 'paper')
+            }
 
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
 def count_participants(client):
-    sendToClients(bytes("Server response: %s participants in chat." % len(clients), "utf8"),'',client)
+    send_str = "Server response: %s participants in chat." % len(clients)
+    print(send_str)
+    sendToClients(bytes(send_str, "utf8"), "", client)
    
 
 def get_participants_names(client):
-    clientNames = []
-    for i in clients:
-        clientNames.append(clients[i])
-    sendToClients(bytes("Server response: Next participants in chat: %s" % clientNames, "utf8"),'',client)
+    clientNames = list(clients.values())
+    send_str = "Server response: Next participants in chat: %s" % clientNames
+    print(send_str)
+    sendToClients(bytes(send_str, "utf8"), "", client)
 
 def current_server_time(client):
     now = datetime.datetime.now()
     serverTime = now.strftime("%d-%m-%Y %H:%M:%S")
-    sendToClients(bytes("Current date and time %s: " % serverTime, "utf8"),'',client)
+    send_str = "Current date and time %s: " % serverTime
+    print(send_str)
+    sendToClients(bytes(send_str, "utf8"), "", client)
+
+def random_value():
+    return random.choice(['paper', 'rock', 'scissors'])
+
+def play_game(user_value, server_value):
+    if user_value not in ['paper', 'rock', 'scissors']:
+        return 'Invalid value. You failed!'
+    if user_value == server_value:
+        return '50/50'
+    if (user_value, server_value) in GAME_WIN:
+        return 'You win!'
+    else:
+        return 'You failed!'
 
 def accept_connections():
     while True:
@@ -37,36 +61,63 @@ def accept_connections():
 
 def handle_client(client):
     name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, press CTRL+C or type {quit} to exit.' % name
+    welcome = "Welcome %s! If you ever want to quit, press CTRL+C or type {quit} to exit." % name
     client.send(bytes(welcome, "utf8"))
     msg = "%s has joined the chat!" % name
     sendToClients(bytes(msg, "utf8"))
     clients[client] = name
+    is_playing_game = False
 
     while True:
-        msg = client.recv(BUFSIZ)
-        if msg == bytes("count_participants", "utf8"):
-            count_participants(client)
-        elif msg == bytes("getNames", "utf8"):
-            get_participants_names(client)
-        elif msg == bytes("getTime", "utf8"):
-            current_server_time(client) 
-        elif msg == bytes("{quit}", "utf8"):
-            client.send(bytes("{quit}", "utf8"))
+        try:
+            msg = client.recv(BUFSIZ).decode("utf-8")
+
+            if is_playing_game:
+                server_value = random_value()
+                game_result = play_game(msg, server_value)
+                send_str = 'Server choose %s value' % server_value
+                print(send_str)
+                client.send(bytes(send_str, "utf8"))
+                print(game_result)
+                client.send(bytes(game_result, "utf8"))
+                is_playing_game = False
+            elif msg == "cmd!count-participants":
+                count_participants(client)
+            elif msg =="cmd!get-names":
+                get_participants_names(client)
+            elif msg == "cmd!get-time":
+                current_server_time(client) 
+            elif msg == "cmd!rock-paper-scissors":
+                send_str = "Let's play game, %s! Enter 'paper', rock' or 'scissors' value" % name
+                print(send_str)
+                client.send(bytes(send_str, "utf8"))
+                is_playing_game = True
+            elif msg == "{quit}":
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                del clients[client]
+                send_str = "%s has left the chat." % name
+                print(send_str)
+                sendToClients(bytes(send_str, "utf8"))
+                break
+            else:
+                print("%s: %s" % (name, msg))
+                sendToClients(bytes(msg, "utf8"), name+": ")
+        except:
             client.close()
             del clients[client]
-            sendToClients(bytes("%s has left the chat." % name, "utf8"))
+            send_str = "%s has left the chat." % name
+            print(send_str)
+            sendToClients(bytes(send_str, "utf8"))
             break
-        else:
-            sendToClients(msg, name+": ")
 
 
 def sendToClients(msg, prefix="", client=""):
     if client:
-        client.send(bytes(prefix, "utf8")+msg)
+        client.send(bytes(prefix, "utf8") + msg)
     else:
         for sock in clients:
-            sock.send(bytes(prefix, "utf8")+msg)
+            sock.send(bytes(prefix, "utf8") + msg)
 
 
 if __name__ == "__main__":
