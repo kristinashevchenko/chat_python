10a491a698831519e3899ac8cc78305ee25581ac
Kristina Shauchenka <kristinashevchenko03@gmail.com>
2020-12-16 16:53:29 +0300

    removed comment

--- pet_shelter/pet_app/admin.py
+++ pet_shelter/pet_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Pet, Animal
 from django.utils.translation import gettext_lazy as _
-from django.urls import path
 
 
 class PetAdmin(admin.ModelAdmin):
@@ -20,12 +19,6 @@ class PetAdmin(admin.ModelAdmin):
     )
 
     list_filter = ('date_of_birth', )
-    # list_display = (
-    #     'name',
-    #     'max_speed',
-    #     'engine_type',
-    #     'transmission'
-    # )
 
     ordering = ('date_of_birth', 'name', )
     search_fields = ('name', 'species', 'sex', )
